566460b065d8c45cb0d68513a9c21ce5cfc72b27
gh286991 <gh286991@mail.com>
2025-05-13 22:21:13 +0800

    類比觸控搖桿

--- script/main.gd
+++ script/main.gd
@@ -6,15 +6,8 @@ var player_velocity: Vector2 = Vector2.ZERO
 
 func _ready():
 	update_score_label()
-	$VirtualJoystick.move_left.connect(_on_move_left)
-	$VirtualJoystick.move_right.connect(_on_move_right)
-	$VirtualJoystick.move_up.connect(_on_move_up)
-	$VirtualJoystick.move_down.connect(_on_move_down)
+	$VirtualJoystick.move_vector.connect(_on_move_vector)
 	$VirtualJoystick.fire.connect(_on_fire)
-	$VirtualJoystick.stop_left.connect(_on_stop_left)
-	$VirtualJoystick.stop_right.connect(_on_stop_right)
-	$VirtualJoystick.stop_up.connect(_on_stop_up)
-	$VirtualJoystick.stop_down.connect(_on_stop_down)
 
 func _process(delta):
 	if player_velocity != Vector2.ZERO:
@@ -24,33 +17,8 @@ func _process(delta):
 		$Player.position.x = clamp($Player.position.x, 0, viewport_rect.size.x)
 		$Player.position.y = clamp($Player.position.y, 0, viewport_rect.size.y)
 
-func _on_move_left():
-	player_velocity.x = -player_speed
-
-func _on_move_right():
-	player_velocity.x = player_speed
-
-func _on_move_up():
-	player_velocity.y = -player_speed
-
-func _on_move_down():
-	player_velocity.y = player_speed
-
-func _on_stop_left():
-	if player_velocity.x < 0:
-		player_velocity.x = 0
-
-func _on_stop_right():
-	if player_velocity.x > 0:
-		player_velocity.x = 0
-
-func _on_stop_up():
-	if player_velocity.y < 0:
-		player_velocity.y = 0
-
-func _on_stop_down():
-	if player_velocity.y > 0:
-		player_velocity.y = 0
+func _on_move_vector(direction: Vector2):
+	player_velocity = direction * player_speed
 
 func _on_fire():
 	if $Player.has_method("shoot"):
--- script/virtual_joystick.gd
+++ script/virtual_joystick.gd
@@ -1,49 +1,59 @@
 extends Control
 
-signal move_left
-signal move_right
-signal move_up
-signal move_down
+signal move_vector(direction: Vector2)
 signal fire
-signal stop_left
-signal stop_right
-signal stop_up
-signal stop_down
+
+var dragging := false
+var max_radius := 80.0
+var base: TextureRect
+var knob: TextureRect
 
 func _ready():
-	$LeftButton.button_down.connect(_on_left_button_pressed)
-	$LeftButton.button_up.connect(_on_left_button_released)
-	$RightButton.button_down.connect(_on_right_button_pressed)
-	$RightButton.button_up.connect(_on_right_button_released)
-	$UpButton.button_down.connect(_on_up_button_pressed)
-	$UpButton.button_up.connect(_on_up_button_released)
-	$DownButton.button_down.connect(_on_down_button_pressed)
-	$DownButton.button_up.connect(_on_down_button_released)
+	base = $JoystickBase
+	knob = $JoystickBase/JoystickKnob
+	_reset_knob()
+	knob.mouse_filter = Control.MOUSE_FILTER_PASS
+	base.mouse_filter = Control.MOUSE_FILTER_PASS
+	knob.connect("gui_input", Callable(self, "_on_knob_gui_input"))
+	base.connect("gui_input", Callable(self, "_on_base_gui_input"))
 	$FireButton.pressed.connect(_on_fire_button_pressed)
 
-func _on_left_button_pressed():
-	emit_signal("move_left")
+func _on_knob_gui_input(event):
+	if event is InputEventScreenTouch or event is InputEventMouseButton:
+		if event.pressed:
+			dragging = true
+			_process_drag(base.get_local_mouse_position())
+		else:
+			dragging = false
+			_reset_knob()
+			emit_signal("move_vector", Vector2.ZERO)
+	elif (event is InputEventScreenDrag or event is InputEventMouseMotion) and dragging:
+		_process_drag(base.get_local_mouse_position())
 
-func _on_left_button_released():
-	emit_signal("stop_left")
+func _on_base_gui_input(event):
+	if event is InputEventScreenTouch or event is InputEventMouseButton:
+		if event.pressed:
+			dragging = true
+			_process_drag(base.get_local_mouse_position())
+		else:
+			dragging = false
+			_reset_knob()
+			emit_signal("move_vector", Vector2.ZERO)
+	elif (event is InputEventScreenDrag or event is InputEventMouseMotion) and dragging:
+		_process_drag(base.get_local_mouse_position())
 
-func _on_right_button_pressed():
-	emit_signal("move_right")
+func _process_drag(local_pos):
+	var center = base.size / 2
+	var v = local_pos - center
+	if v.length() > max_radius:
+		v = v.normalized() * max_radius
+	knob.position = center + v - knob.size / 2
+	var direction = v / max_radius
+	emit_signal("move_vector", direction)
 
-func _on_right_button_released():
-	emit_signal("stop_right")
-
-func _on_up_button_pressed():
-	emit_signal("move_up")
-
-func _on_up_button_released():
-	emit_signal("stop_up")
-
-func _on_down_button_pressed():
-	emit_signal("move_down")
-
-func _on_down_button_released():
-	emit_signal("stop_down")
+func _reset_knob():
+	var center = $JoystickBase.size / 2
+	$JoystickBase/JoystickKnob.position = center - $JoystickBase/JoystickKnob.size / 2
 
 func _on_fire_button_pressed():
 	emit_signal("fire") 
